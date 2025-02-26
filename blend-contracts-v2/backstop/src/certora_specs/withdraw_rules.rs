fa192490c5d6c282e25a6ac3fb0e7533ce958c11
Chandrakana Nandi <chandra.nandi90@gmail.com>
2025-02-26 11:35:38 -0800

    use release

--- blend-contracts-v2/backstop/src/certora_specs/withdraw_rules.rs
+++ blend-contracts-v2/backstop/src/certora_specs/withdraw_rules.rs
@@ -7,7 +7,7 @@ use crate::backstop::{execute_queue_withdrawal, execute_withdraw};
 // shares to withdraw must be nonnegative
 #[rule]
 pub fn amount_to_withdraw_nonnegative(e: &Env, from: Address, pool_address: Address, shares: i128) {
-    // cvlr_assume!(shares < 0);
+    cvlr_assume!(shares < 0);
     execute_withdraw(e, &from, &pool_address, shares);
     cvlr_assert!(false); // should pass when assumption is enabled, fail otherwise
 }
@@ -16,7 +16,7 @@ pub fn amount_to_withdraw_nonnegative(e: &Env, from: Address, pool_address: Addr
 // needs -smt_preciseBitwiseOps
 #[rule]
 pub fn withdraw_queue_only_positive(e: Env, from: Address, pool_address: Address, amount: i128) {
-    // cvlr_assume!(amount < 0);
+    cvlr_assume!(amount < 0);
     execute_queue_withdrawal(&e, &from, &pool_address, amount);
     cvlr_assert!(false); // should pass when assumption is enabled, fail otherwise
 }
