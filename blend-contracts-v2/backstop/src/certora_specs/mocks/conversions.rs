856a3496350bde441c7236967110ad0370b5bdec
Chandrakana Nandi <cnandi@cs.washington.edu>
2025-03-09 06:52:36 -0700

    Update conversions.rs

--- blend-contracts-v2/backstop/src/certora_specs/mocks/conversions.rs
+++ blend-contracts-v2/backstop/src/certora_specs/mocks/conversions.rs
@@ -3,14 +3,14 @@ pub(crate) fn certora_convert_to_tokens(pool_shares: i64, pool_tokens: i64, shar
     if pool_shares == 0 {
         return shares;
     }
-    shares * pool_shares / pool_tokens
+    shares * pool_tokens / pool_shares
 }
 
 pub(crate) fn certora_convert_to_shares(pool_shares: i64, pool_tokens: i64, tokens: i64) -> i64 {
     if pool_shares == 0 {
         return tokens;
     }
-    tokens * pool_tokens / pool_shares
+    tokens * pool_shares / pool_tokens
 }
 
 /////////////
