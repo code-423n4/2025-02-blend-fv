48576dd940d47560de60d14c8587b4d437e4bb42
Chandrakana Nandi <cnandi@cs.washington.edu>
2025-03-09 20:32:30 -0700

    update 64 bit version conversions.rs

--- blend-contracts-v2/backstop/src/certora_specs/mocks/conversions.rs
+++ blend-contracts-v2/backstop/src/certora_specs/mocks/conversions.rs
@@ -1,16 +1,27 @@
+use soroban_fixed_point_math::FixedPoint;
+
 /////////////
 pub(crate) fn certora_convert_to_tokens(pool_shares: i64, pool_tokens: i64, shares: i64) -> i64 {
     if pool_shares == 0 {
         return shares;
     }
-    shares * pool_tokens / pool_shares
+    // shares * pool_tokens / pool_shares
+    
+    shares
+        .fixed_mul_floor(pool_tokens, pool_shares)
+        .unwrap()
 }
 
 pub(crate) fn certora_convert_to_shares(pool_shares: i64, pool_tokens: i64, tokens: i64) -> i64 {
     if pool_shares == 0 {
         return tokens;
     }
-    tokens * pool_shares / pool_tokens
+    // tokens * pool_shares / pool_tokens
+
+    tokens
+        .fixed_mul_floor(pool_shares, pool_tokens)
+        .unwrap()
 }
+
 
 /////////////
